3a987b6c397695298a4b2533b234ba7484970792
Jihun Kim <devgrapher@gmail.com>
2020-10-27 11:47:28 +0900

    Update SEO title

--- src/components/seo.js
+++ src/components/seo.js
@@ -32,8 +32,8 @@ function SEO({ description, lang, meta, keywords, title }) {
       htmlAttributes={{
         lang,
       }}
-      title={title}
-      titleTemplate={`%s | ${site.siteMetadata.title}`}
+      title={title || site.siteMetadata.title}
+      titleTemplate={`%s | Home`}
       meta={[
         {
           name: `description`,
@@ -84,7 +84,7 @@ function SEO({ description, lang, meta, keywords, title }) {
 SEO.defaultProps = {
   lang: `kr`,
   meta: [],
-  keywords: [],
+  keywords: ["멸종저항", "extinction rebellion", "기후변화", "기후위기", "비폭력시민불복종운동", "사회운동", "기후위기비상행동"],
 };
 
 SEO.propTypes = {
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,9 @@ import SEO from "../components/seo";
 import Title from "../components/title";
 import Content from "../components/content";
 
-const SEO_KEWORDS = ["멸종저항", "extinction rebellion", "기후변화", "기후위기", "비폭력시민불복종운동", "사회운동", "기후위기비상행동"];
-
 const IndexPage = () => (
   <Layout>
-    <SEO keywords={SEO_KEWORDS}/>
+    <SEO />
     <Title />
     <Content />
   </Layout>
