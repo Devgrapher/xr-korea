c92729405608c66dc208c9bd08520dc4fe27af90
Jihun Kim <devgrapher@gmail.com>
2020-11-20 16:15:14 +0900

    Replace logo image

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 import styled from "styled-components";
 import { COLORS } from "../styles/constants";
-import logo from "../images/logo.png";
+import logo from "../images/logo.svg";
 import title from "../images/title.png";
 import instagram from "../images/instagram.svg";
 
