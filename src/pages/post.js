f94162f5a355fad3160a7032a05abebb394e3b99
Jihun Kim <devgrapher@gmail.com>
2021-05-05 17:34:12 +0900

    Fix post alignment

--- src/pages/post.js
+++ src/pages/post.js
@@ -10,7 +10,7 @@ import styled from 'styled-components';
 const PostContainer = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: space-between;
+  justify-content: flex-start;
   flex-wrap: wrap;
   margin-bottom: 9px;
   &:after {
